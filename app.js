3d6992dc028c03ee0d7e66e6e9af279b7af5d678
= <archanrd29@gmail.com>
2022-09-15 20:59:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let enrollments = [];
+
+let roll = 1;
+function register(){   
+    let tname = document.getElementById('name').value;
+    let temail = document.getElementById('email').value;
+
+
+    let enrollCount = enrollments.filter((x)=>{
+        return x.email == temail;
+    })
+
+    if(enrollCount.length == 0){
+        let registration = {
+            rollNo: roll,
+            name: tname,
+            email: temail,
+        }
+
+        roll+=1;
+        enrollments.push(registration);
+
+
+        tname.value = "";
+        temail.value = "";
+
+    }else{
+        console.log('email exists');
+    }
+
+    console.log(enrollments);
+}
+
+function checkEmail(){
+    let temail = document.getElementById('email').value;
+    
+    let mainCheck = enrollments.filter((x)=>{
+        return x.email == temail;
+    })
+
+    if(mainCheck.length == 0){
+        // console.log();
+    }else{
+        console.log('Email Exists');
+    }
+}
