287986779b7d35bcdc8fbfbc17989f0b90c71d61
= <archanrd29@gmail.com>
2022-09-16 20:20:10 +0530

    sg

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let roll = 1;
 function register(){   
     let tname = document.getElementById('name').value;
     let temail = document.getElementById('email').value;
+    let table = document.getElementById('table');
 
 
     let enrollCount = enrollments.filter((x)=>{
@@ -17,13 +18,27 @@ function register(){
             email: temail,
         }
 
-        roll+=1;
         enrollments.push(registration);
+
+        //display data
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+
+        td1.innerHTML = roll;
+        td2.innerHTML = tname;
+        td2.innerHTML = temail;
+
+        table.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
 
         document.getElementById('name').value = "";
         document.getElementById('email').value = "";
         
-
+        roll+=1;
     }else{
         console.log('email exists');
     }
